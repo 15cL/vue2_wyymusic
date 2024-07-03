53b85c7510fc834a82684df0d8e15ab919f34e12
Luo Shi Fa <1246449573@qq.com>
2024-07-04 00:54:40 +0800

    调试 20240704v2

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: './',
   transpileDependencies: true,
   devServer: {
     proxy: {
@@ -14,11 +15,11 @@ module.exports = defineConfig({
          *   eg:
          *        上面 target: '【http】://192.168.2.127:80'就是http协议
          */
-        pathRewrite: { // 重写路径 替换请求地址中的指定路径
+        pathRewrite: {
+          // 重写路径 替换请求地址中的指定路径
           '^/api': '/' // 将请求地址中的api替换为空
         }
       }
     }
   }
-
 })
