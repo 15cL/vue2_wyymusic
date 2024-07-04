99948accfb330a7f26ad4fc249b8f46e57e9faaf
Luo Shi Fa <1246449573@qq.com>
2024-07-04 09:12:43 +0800

    调试 20240703v2

--- vue.config.js
+++ vue.config.js
@@ -6,16 +6,6 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': {
-        target: process.env.VUE_APP_BASE_API, // 代理的接口地址
-        changeOrigin: false, // 允许跨域
-        secure: true,
-        /** secure说明
-         * 若接口地址为http协议，配置secure: false,
-         * 若接口地址为https协议，配置secure: true,
-         *   一般情况下，配置secure: false,
-         *   eg:
-         *        上面 target: '【http】://192.168.2.127:80'就是http协议
-         */
         pathRewrite: {
           // 重写路径 替换请求地址中的指定路径
           '^/api': '/' // 将请求地址中的api替换为空
